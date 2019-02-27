c0edbafb524c605fc89b0615dd6db33fb958bfc9
Zzzzzyk <Zzzzzyk@users.noreply.github.com>
2019-02-27 12:13:34 +0800

    combine
    combine

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
 from app import app
+from app.models import *
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
+
+
+migrate=Migrate(app,db)
+manager = Manager(app)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
-
+    manager.run()
