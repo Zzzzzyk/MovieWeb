c6b81190351b324319aba4a29f3ee2b882f1c3b8
Zzzzzyk <Zzzzzyk@users.noreply.github.com>
2019-02-27 13:21:45 +0800

    add

--- app/models.py
+++ app/models.py
@@ -178,20 +178,20 @@ class Oplog(db.Model):
     def __repr__(self):
         return "<Oplog %r>" % self.id
 
-from app import app
-from flask_migrate import Migrate,MigrateCommand
-from flask_script import Manager
-migrate = Migrate(app,db)
-manager = Manager(app)
-manager.add_command('db',MigrateCommand)
-if __name__ == '__main__':
-    db.create_all()
-    superuser = Admin(
-    name='zzzzzyk',
-    pwd='zyk666!!',
-    is_super=0,
-    role_id='',
-    )
-    db.session.add(superuser)
-    db.session.commit()
-    manager.run()
+# from app import app
+# from flask_migrate import Migrate,MigrateCommand
+# from flask_script import Manager
+# migrate = Migrate(app,db)
+# manager = Manager(app)
+# manager.add_command('db',MigrateCommand)
+# if __name__ == '__main__':
+#     db.create_all()
+#     superuser = Admin(
+#     name='zzzzzyk',
+#     pwd='zyk666!!',
+#     is_super=0,
+#     role_id='',
+#     )
+#     db.session.add(superuser)
+#     db.session.commit()
+#     manager.run()
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app,db
 from app.models import *
 from flask_migrate import Migrate,MigrateCommand
 from flask_script import Manager
@@ -9,4 +9,13 @@ manager = Manager(app)
 manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
+    db.create_all()
+    superuser = Admin(
+    name='zzzzzyk',
+    pwd='zyk666!!',
+    is_super=0,
+    role_id='',
+    )
+    db.session.add(superuser)
+    db.session.commit()
     manager.run()
